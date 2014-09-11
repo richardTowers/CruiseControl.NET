29fdf4060039dac60373cee326fd626bca7804a8
Richard Towers <richard@richard-towers.com>
2014-09-11 19:47:13 +0100

    Add an HttpHandler that can handle simultaneous requests
    Asp.NET will queue requests (for a single session) unless the HttpHandler they hit implements IReadOnlySessionState.
    Server Sent Events hold a request open semi-permanently, so any subsequent requests would just sit in the queue waiting
    for the SSE request to finish.
    
    I've added an new handler just for this, which feels pretty heavy handed. I'm also not thrilled about overriding .ashx, but "when in rome".
    
    If anyone has a better idea for this I'd be thrilled to hear it!

---
+++ project/WebDashboard/MVC/ASPNET/ReadonlySessionStateHttpHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.SessionState;
+using System.Web;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.MVC.ASPNET
+{
+    /// <summary>
+    /// This HttpHandler only provides read-only access to session state
+    /// which allows Asp.NET to handle multiple concurrent requests.
+    /// </summary>
+    /// <remarks>
+    /// At the moment we're not implementing anything here, we're just
+    /// adding the <see cref="IReadOnlySessionState"/> to <see cref="HttpHandler"/>.
+    /// See <see cref="HttpHandler"/> for all method implementations.
+    /// </remarks>
+    public class ReadonlySessionStateHttpHandler : HttpHandler, IReadOnlySessionState
+    {
+
+    }
+}
